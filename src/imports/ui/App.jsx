69db7799016d559ad76c1785bb7568ca167d3a3e
Leo Schneider <leocoelho256@gmail.com>
2023-03-24 21:39:15 -0300

    1.4 - Create Sample Tasks

--- src/imports/ui/App.jsx
+++ src/imports/ui/App.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
 
+/* Providing this tasks before make use in the DB */
+const tasks = [
+  { _id: 1, text: 'First Task' },
+  { _id: 2, text: 'Second Task' },
+  { _id: 3, text: 'Third Task' },
+];
+
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
