f1b29102060cd2ff36a98d4c118d3308e04f0e9a
Leo Schneider <leocoelho256@gmail.com>
2023-03-24 21:44:21 -0300

    using my creativity

--- src/imports/ui/App.jsx
+++ src/imports/ui/App.jsx
@@ -3,10 +3,11 @@ import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
 
 /* Providing this tasks before make use in the DB */
+/* The tutorial man say that I can be creative with taks. I will put bakery tasks */
 const tasks = [
-  { _id: 1, text: 'First Task' },
-  { _id: 2, text: 'Second Task' },
-  { _id: 3, text: 'Third Task' },
+  { _id: 1, text: 'Prepare French Bread' },
+  { _id: 2, text: 'Put Breads to Weight' },
+  { _id: 3, text: 'Sell All Breads' },
 ];
 
 export const App = () => (
