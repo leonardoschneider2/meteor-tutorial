3507c5d5d7c8007fc50fa0ed22cbaf3201c0ff92
Leo Schneider <leocoelho256@gmail.com>
2023-03-25 17:03:12 -0300

    restarting tutorial app

--- src/imports/ui/Task.jsx
+++
@@ -1,9 +0,0 @@
-import react from 'react';
-
-export const Task = ({ task }) => {
-  return (
-    <li>
-      { task.text }
-    </li>
-  )
-};
--- src/server/main.js
+++
@@ -1,9 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-
-async function insertLink({ title, url }) {
-  await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
-}
-
-Meteor.startup(async () => {
-  /* Links was deleted */
-});
